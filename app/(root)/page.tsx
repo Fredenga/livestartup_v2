73a9c213096edcfd90676bff92251e104dce0cc4
fredenga <dengafred@gmail.com>
2025-07-28 09:19:33 -0700

    integrate shadcn, code hero

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,11 @@
 
 export default function Home() {
   return (
-   <div>
-    Welcome to the world
-   </div>
+   <section className={"pink_container"}>
+    <h1 className={"heading"}>Pitch Your Startup <br/> Connect with Entrepreneurs</h1>
+    <p className={"sub-heading max-w-3xl"}>
+        Submit ideas, Vote on Pitches and get noticed in Virtual Competitions
+    </p>
+   </section>
   );
 }
