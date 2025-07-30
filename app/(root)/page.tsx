e06807840ab8d316f7a2d490300daad1e26319ea
fredenga <dengafred@gmail.com>
2025-07-30 05:22:10 -0700

    implement sanity fetch logic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,20 +1,11 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard"
+import {client} from "@/sanity/lib/client";
+import {STARTUPS_QUERY} from "@/sanity/lib/queries";
 export default async function Home({searchParams}: {searchParams: Promise<{query?: string}>}) {
   const query = (await searchParams).query;
-  const posts = [{
-      _createdAt: new Date(),
-      views: 55,
-      author: {
-          _id: 1,
-          name: "Albert Stark"
-      },
-      _id: 1,
-      description: "This is a description",
-      image: "https://images.unsplash.com/photo-1593376853899-fbb47a057fa0?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cm9ib3RzfGVufDB8fDB8fHww",
-      category: "Robots",
-      title: "We Robots"
-  }]
+
+  const posts = await client.fetch(STARTUPS_QUERY)
     return (
         <>
            <section className={"pink_container"}>
