c3ccd9d8ac20fbe35d443308e32d8263ecd65d23
fredenga <dengafred@gmail.com>
2025-07-31 00:56:23 -0700

    add query startup by ID

---
+++ app/(root)/startup/[id]/page.tsx
@@ -0,0 +1,10 @@
+const Page = async ({params}: {params: Promise<{id: string}>}) => {
+    const id = (await params).id
+    return (
+        <>
+            <h1>This is a startup number {id}</h1>
+        </>
+    );
+};
+
+export default Page;
